70567824a9e3cacfa04fc16f5995712056c36cea
Andrew Fowler <andrew.fowler@netnook.net>
2024-03-11 21:40:47 +0100

    Prettify

--- qmk/keymaps/default/keymap.c
+++ qmk/keymaps/default/keymap.c
@@ -2,7 +2,7 @@
 
 #include QMK_KEYBOARD_H
 
-enum dasbob_layers {
+enum pfm_layers {
   _ALPHA,
   _LANG,
   _NUM,
@@ -56,59 +56,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
     [_LANG] = LAYOUT_split_46513(
-                KC_TRNS,   XXXXXXX,      XXXXXXX,  XXXXXXX,                            XXXXXXX,      XX_UMLAUT_U,  XXXXXXX,      KC_TRNS,
-        XXXXXXX, XXXXXXX,  XX_UMLAUT_A,  KC_LSFT,  XXXXXXX,  XXXXXXX,        XXXXXXX,  XX_ESZETT,    XX_EURO,      XX_UMLAUT_O,  XXXXXXX, XXXXXXX,
-                XXXXXXX,   XXXXXXX,      XXXXXXX,  XXXXXXX,  XXXXXXX,        XXXXXXX,  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX, 
-                                     XXXXXXX,                                                             XXXXXXX,
-                                KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,  KC_TRNS,      KC_TRNS
+                 KC_TRNS,   XXXXXXX,      XXXXXXX,  XXXXXXX,                         XXXXXXX,      XX_UMLAUT_U,  XXXXXXX,      KC_TRNS,
+        XXXXXXX, XXXXXXX,   XX_UMLAUT_A,  KC_LSFT,  XXXXXXX,  XXXXXXX,     XXXXXXX,  XX_ESZETT,    XX_EURO,      XX_UMLAUT_O,  XXXXXXX, XXXXXXX,
+                 XXXXXXX,   XXXXXXX,      XXXXXXX,  XXXXXXX,  XXXXXXX,     XXXXXXX,  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX, 
+                                          XXXXXXX,                                                 XXXXXXX,
+                                          KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,  KC_TRNS,      KC_TRNS
     ),
 
     [_NUM] = LAYOUT_split_46513(
-                KC_TRNS,  XXXXXXX,   XXXXXXX,       KC_DEL,                                KC_7,          KC_8,          KC_9,          KC_TRNS,
+                 KC_TRNS,  XXXXXXX,   XXXXXXX,       KC_DEL,                                KC_7,          KC_8,          KC_9,          KC_TRNS,
         XXXXXXX, KC_LGUI,  XX_CTLA,   KC_LSFT,       XX_TAB,   C(KC_V),        KC_PERCENT,  RCTL_T(KC_4),  RSFT_T(KC_5),  LALT_T(KC_6),  XX_PI,    XXXXXXX,
-                C(KC_X),  C(KC_Y),   C(KC_Z),       C(KC_S),  C(KC_C),        KC_0,        KC_1,          KC_2,          KC_3,          XXXXXXX,
-                                     XXXXXXX,                                                             XXXXXXX,
-                                     KC_TRNS,       KC_TRNS,  KC_TRNS,        KC_COMMA,    KC_DOT,        KC_TRNS
+                 C(KC_X),  C(KC_Y),   C(KC_Z),       C(KC_S),  C(KC_C),        KC_0,        KC_1,          KC_2,          KC_3,          XXXXXXX,
+                                      XXXXXXX,                                                             XXXXXXX,
+                                      KC_TRNS,       KC_TRNS,  KC_TRNS,        KC_COMMA,    KC_DOT,        KC_TRNS
     ),
 
    [_SYMBOLS] = LAYOUT_split_46513(
-                KC_TRNS,      KC_EQUAL,     KC_QUESTION,  KC_EXCLAIM,                                     KC_LEFT_BRACKET,      KC_RIGHT_BRACKET,      KC_GRAVE,         KC_TRNS,
+                 KC_TRNS,      KC_EQUAL,     KC_QUESTION,  KC_EXCLAIM,                                     KC_LEFT_BRACKET,      KC_RIGHT_BRACKET,      KC_GRAVE,         KC_TRNS,
         XXXXXXX, XX_AMPERSAND, XX_SLASH,     KC_MINUS,     XX_COLON,        KC_DOLLAR,     KC_CIRCUMFLEX,  XX_LEFT_PAREN,        KC_RIGHT_PAREN,        XX_DOUBLE_QUOTE,  XX_LEFT_ANGLE_BRACKET,   XXXXXXX,
-                KC_PIPE,      KC_ASTERISK,  KC_PLUS,      KC_SEMICOLON,    KC_BACKSLASH,  KC_TILDE,       KC_LEFT_CURLY_BRACE,  KC_RIGHT_CURLY_BRACE,  KC_QUOTE,         KC_RIGHT_ANGLE_BRACKET, 
-                                     XXXXXXX,                                                             XXXXXXX,
-                                            KC_AT,        KC_UNDERSCORE,   KC_HASH,       KC_TRNS,        KC_TRNS,              KC_TRNS
+                 KC_PIPE,      KC_ASTERISK,  KC_PLUS,      KC_SEMICOLON,    KC_BACKSLASH,  KC_TILDE,       KC_LEFT_CURLY_BRACE,  KC_RIGHT_CURLY_BRACE,  KC_QUOTE,         KC_RIGHT_ANGLE_BRACKET, 
+                                             XXXXXXX,                                                                            XXXXXXX,
+                                             KC_AT,        KC_UNDERSCORE,   KC_HASH,       KC_TRNS,        KC_TRNS,              KC_TRNS
      ),
 
     [_BUTTONS] = LAYOUT_split_46513(
-                KC_TRNS,  KC_ESC,           QK_CAPS_WORD_TOGGLE,  KC_CAPS_LOCK,                        KC_F7,          KC_F8,          KC_F9,          KC_TRNS,
+                 KC_TRNS,  KC_ESC,           QK_CAPS_WORD_TOGGLE,  KC_CAPS_LOCK,                        KC_F7,          KC_F8,          KC_F9,          KC_TRNS,
         XXXXXXX, KC_LGUI,  KC_LALT,          KC_LSFT,              KC_LCTL,       XXXXXXX,  XXXXXXX,    RCTL_T(KC_F4),  RSFT_T(KC_F5),  LALT_T(KC_F6),  KC_F11,   XXXXXXX,
-                XXXXXXX,  KC_PRINT_SCREEN,  KC_EXECUTE,           KC_HELP,       XXXXXXX,  KC_F10,     KC_F1,          KC_F2,          KC_F3,          KC_F12,
-                                     XXXXXXX,                                                             XXXXXXX,
-                                            KC_TRNS,              KC_TRNS,       KC_TRNS,  KC_TRNS,    KC_TRNS,        KC_TRNS
+                 XXXXXXX,  KC_PRINT_SCREEN,  KC_EXECUTE,           KC_HELP,       XXXXXXX,  KC_F10,     KC_F1,          KC_F2,          KC_F3,          KC_F12,
+                                             XXXXXXX,                                                                   XXXXXXX,
+                                             KC_TRNS,              KC_TRNS,       KC_TRNS,  KC_TRNS,    KC_TRNS,        KC_TRNS
     ),
 
     [_NAV] = LAYOUT_split_46513(
-                XXXXXXX,   XX_WS_LEFT,  XXXXXXX,         XX_WS_RIGHT,                         KC_HOME,    KC_UP,     KC_END ,      XXXXXXX,
-        XXXXXXX, KC_LGUI,   KC_LALT,     LSFT_T(KC_ESC),  LCTL_T(KC_TAB),  XXXXXXX,  XXXXXXX,  KC_LEFT,    KC_DOWN,   KC_RIGHT,     KC_MENU,          XXXXXXX,
-                XXXXXXX,   XX_WS1,      XX_WS2,          XX_WS3,          XX_WS4,   XXXXXXX,  KC_PAGE_UP, KC_INSERT, KC_PAGE_DOWN, KC_APPLICATION,
-                                     XXXXXXX,                                                             XXXXXXX,
-                                        KC_TRNS,         KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS
+                 XXXXXXX,   XX_WS_LEFT,  XXXXXXX,         XX_WS_RIGHT,                         KC_HOME,     KC_UP,     KC_END ,      XXXXXXX,
+        XXXXXXX, KC_LGUI,   KC_LALT,     LSFT_T(KC_ESC),  LCTL_T(KC_TAB),  XXXXXXX,  XXXXXXX,  KC_LEFT,     KC_DOWN,   KC_RIGHT,     KC_MENU,          XXXXXXX,
+                 XXXXXXX,   XX_WS1,      XX_WS2,          XX_WS3,          XX_WS4,   XXXXXXX,  KC_PAGE_UP,  KC_INSERT, KC_PAGE_DOWN, KC_APPLICATION,
+                                         XXXXXXX,                                                           XXXXXXX,
+                                         KC_TRNS,         KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS
     ),
 
    [_MEDIA] = LAYOUT_split_46513(
-                XXXXXXX,    XX_BOX_DR,  PB_8,     PB_9,                        KC_KB_VOLUME_UP,             KC_KB_MUTE,                   KC_KB_VOLUME_DOWN,            XXXXXXX,
+                 XXXXXXX,    XX_BOX_DR,  PB_8,     PB_9,                        KC_KB_VOLUME_UP,             KC_KB_MUTE,                   KC_KB_VOLUME_DOWN,            XXXXXXX,
         XXXXXXX, XXXXXXX,    PB_4,       PB_5,     PB_6,    XXXXXXX,  XXXXXXX,  RCTL_T(KC_MEDIA_PREV_TRACK), RSFT_T(KC_MEDIA_PLAY_PAUSE),  LALT_T(KC_MEDIA_NEXT_TRACK),  RGUI_T(XXXXXXX),  XXXXXXX,
-                XXXXXXX,    PB_1,       PB_2,     PB_3,    XXXXXXX,  XXXXXXX,  XXXXXXX,                     XXXXXXX,                      XXXXXXX,                      XXXXXXX,
-                                     XXXXXXX,                                                             XXXXXXX,
-                                        KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS
+                 XXXXXXX,    PB_1,       PB_2,     PB_3,    XXXXXXX,  XXXXXXX,  XXXXXXX,                     XXXXXXX,                      XXXXXXX,                      XXXXXXX,
+                                         XXXXXXX,                                                            XXXXXXX,
+                                         KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS
     ),
              
     [_MOUSE] = LAYOUT_split_46513(
-               XXXXXXX,  XXXXXXX,        OSL(_SYMBOLS),  OSL(_BUTTONS),                          KC_MS_WH_UP,   KC_MS_UP,      KC_MS_WH_DOWN,  XXXXXXX,
+                XXXXXXX,  XXXXXXX,        OSL(_SYMBOLS),  OSL(_BUTTONS),                          KC_MS_WH_UP,   KC_MS_UP,      KC_MS_WH_DOWN,  XXXXXXX,
        XXXXXXX, KC_LGUI,  KC_LALT,        KC_LSFT,        KC_LCTL,        XXXXXXX,  XXXXXXX,      KC_MS_LEFT,    KC_MS_DOWN,    KC_MS_RIGHT,    XXXXXXX,  XXXXXXX,
-               XXXXXXX,  G(A(C(KC_W))),  G(A(C(KC_C))),  G(A(C(KC_F))),  XXXXXXX,  XXXXXXX,      XX_MS_BTN1,    XX_MS_BTN2,    XX_MS_BTN3,     KC_RGUI,
-                                     XXXXXXX,                                                             XXXXXXX,
-                                         KC_TRNS,        KC_TRNS,        KC_TRNS,  KC_MS_ACCEL0, KC_MS_ACCEL1,  KC_MS_ACCEL2
+                XXXXXXX,  G(A(C(KC_W))),  G(A(C(KC_C))),  G(A(C(KC_F))),  XXXXXXX,  XXXXXXX,      XX_MS_BTN1,    XX_MS_BTN2,    XX_MS_BTN3,     KC_RGUI,
+                                          XXXXXXX,                                                               XXXXXXX,
+                                          KC_TRNS,        KC_TRNS,        KC_TRNS,  KC_MS_ACCEL0, KC_MS_ACCEL1,  KC_MS_ACCEL2
     ),
 };
 
